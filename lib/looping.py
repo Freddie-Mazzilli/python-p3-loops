6dd72bf7a6f7e8e3abca274ba1d6fcfa9c1fed41
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-05-23 14:32:52 -0400

    Completes python loops lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10):
+        print(f"{10 - i}")
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for i in int_list:
+        squared_list.append(i ** 2)
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    fizzbuzz_list = []
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            fizzbuzz_list.append('FizzBuzz')
+        elif i % 3 == 0:
+            fizzbuzz_list.append('Fizz')
+        elif i % 5 == 0:
+            fizzbuzz_list.append('Buzz')
+        else:
+            fizzbuzz_list.append(str(i))
+    for item in fizzbuzz_list:
+        print(item)
